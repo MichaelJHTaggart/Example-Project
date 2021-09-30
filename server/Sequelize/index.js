015dddc40ec307525e0c8bbca4552d4a28b1f0d3
Michael Taggart <mtaggarthawk@gmail.com>
2021-09-30 23:32:31 +0200

    updated some notes

--- server/Sequelize/index.js
+++ server/Sequelize/index.js
@@ -1,40 +1,38 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
-const {CONNECTION_STRING} = process.env
+const { CONNECTION_STRING } = process.env
 const sequelize = new Sequelize(CONNECTION_STRING, {
-    //for using my heroku uri I needed this setup
-    // to connect with other database hosts it could be different
-    //or if hosting youself it yourself it will be a differnt setup
-    dialect: "postgres",
-    define: {
-      freezeTableName: true
+  //for using my heroku uri I needed this setup
+  // to connect with other database hosts it could be different
+  //or if hosting youself it yourself it will be a differnt setup
+  dialect: "postgres",
+  define: {
+    freezeTableName: true
+  },
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
     },
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  });
+  },
+});
 
-sequelize
-  .sync({
-    logging: console.log,
-  })
-  .then(()=>{
-  
-    console.log("DB connected !");
-  })
+//* I have commented out the below code so to improve speed of application. The table has been created in the database. This prevents from needing to run this function for every time we use sequelize.
+// sequelize
+//   .sync({
+//     logging: console.log,
+//   })
+//   .then(() => {
+//     console.log("DB connected !");
+//   })
+//   .catch((err) => {
+//     console.error("DB NOT CONNECTED =O ", err);
+//   });
 
-  .catch((err) => {
-    console.error("DB NOT CONNECTED =O ", err);
-  });
-
-const db ={
-  Sequelize:Sequelize,
-  sequelize:sequelize,
+const db = {
+  Sequelize: Sequelize,
+  sequelize: sequelize,
   user: require('./userModel')(sequelize, Sequelize)
 }
-
 
 module.exports = db
